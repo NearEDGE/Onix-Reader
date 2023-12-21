f931f1ec40950f5fb80aca804c9e83880df71b88
User <User@LAPTOP-EFFM26D3>
2023-12-20 23:42:19 -0500

    Add project files.

---
+++ Gen 1 Save Classes/Gen1SaveFile.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gen_1_Save_Classes
+{
+    internal class Gen1SaveFile
+    {
+        public static DataBlockInfo PlayerName = new DataBlockInfo(0x2598, 0x0B),
+            MainData = new DataBlockInfo(0x25A3, 0x789),
+            SpriteData = new DataBlockInfo(0x2D2C, 0x200),
+            PartyData = new DataBlockInfo(0x2F2C, 0x194),
+            CurrentBoxData = new DataBlockInfo(0x30C0, 0x462),
+            TilesetType = new DataBlockInfo(0x3522, 0x1),
+            MainDataChecksum = new DataBlockInfo(0x3523, 0x1);
+
+
+        byte[] data;
+
+        public byte Checksum
+        { get {  return data[0x3523]; } }
+
+        public Gen1SaveFile(string filename)
+        {
+            data = File.ReadAllBytes(filename);
+        }
+
+        public byte[] Data
+            { get { return data; } }
+
+        internal byte GenerateBank1Checksum()
+        {
+            byte checksum = 0x00;
+
+            for(int i = 0x2598; i!= 0x3523; ++i)
+                checksum = (byte)(checksum + data[i]);
+
+            checksum = (byte)~checksum;
+
+            return checksum;
+        }
+
+        public void UpdateChecksum()
+        {
+            data[0x3523] = GenerateBank1Checksum();
+        }
+
+    }
+
+    internal struct DataBlockInfo
+    {
+        UInt16 offset, size;
+
+        public DataBlockInfo(int _offset, int _size)
+        {
+            offset = (UInt16)_offset;
+            size = (UInt16)_size;
+        }
+
+        public UInt16 Offset
+            { get { return offset; } }
+        public UInt16 Size
+            { get { return size; } }
+    }
+}
---
+++ Gen 1 Save Classes/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.IO;
+
+namespace Gen_1_Save_Classes
+{
+    internal class Program
+    {
+        static string[]? PokemonNames;
+
+        static void Main(string[] args)
+        {
+            PokemonNames = File.ReadAllLines("Gen1 Pokemon Names.txt");
+
+
+            Gen1SaveFile PKMNBLUE = new Gen1SaveFile("POKEMON BLUE.sav");
+            Gen1SaveFile PKMNRED = new Gen1SaveFile("POKEMON RED.sav");
+
+
+
+            byte[] PokemonOwned = File.ReadAllBytes(@"C:\Users\User\source\repos\arduinogdump-communicator\GBCartRead-Communicator\bin\Debug\RBYPokedex.dat");
+
+
+            ShowPokemonList(PokemonOwned);
+
+
+            /*
+            byte[] MixedData, Datacomp = MixedData = PokemonOwnedBlue;
+
+
+            byte curChecksum = (byte)(~PKMNBLUE.Checksum);
+
+            for (int i = 0; i != 0x26; ++i)
+            {
+                MixedData[i] |= PokemonOwnedRed[i];
+
+                curChecksum -= PokemonOwnedBlue[i];
+                curChecksum += MixedData[i];// (byte)((PokemonOwnedBlue[i] ^ PokemonOwnedRed[i]) & PokemonOwnedRed[i]);
+            }
+
+            curChecksum = (byte)~curChecksum;
+
+            // Mix Records
+            for (int i = 0; i != 0x13; ++i)
+            {
+                PKMNBLUE.Data[0x25A3 + i] |= PKMNRED.Data[0x25A3 + i];
+                PKMNBLUE.Data[0x25B6 + i] |= PKMNRED.Data[0x25B6 + i];
+            }
+
+            PKMNBLUE.UpdateChecksum();
+
+            if (curChecksum != PKMNBLUE.Checksum)
+                Console.WriteLine("\r\n\r\nChecksum mismatch:\r\n\r\n    Expected: 0x{0:X}\r\n         Got: 0x{1:X}\r\n\r\n", (byte)~PKMNBLUE.Checksum, (byte)~curChecksum);
+
+
+            File.WriteAllBytes("POKEMON BLUE test.sav", PKMNBLUE.Data);
+
+            Console.WriteLine("Done.");//*/
+
+        }
+
+        static bool[] GetPokemonOwnedList(byte[] pokemonOwnedData)
+        {
+            bool[] output = new bool[152];
+
+            for (int i = 0; i != 0x13; ++i)
+                for (int j = 0; j != 8; ++j)
+                    output[i*8 + j] = ((pokemonOwnedData[i] >> j) & 0x01) == 1;
+
+            return output;
+        }
+
+        static bool[] GetPokemonUnownedList(byte[] pokemonOwnedData)
+        {
+            bool[] output = new bool[152];
+
+            for (int i = 0; i != 0x13; ++i)
+                for (int j = 0; j != 8; ++j)
+                    output[i*8 + j] = ((pokemonOwnedData[i] >> j) & 0x01) == 0;
+
+            return output;
+        }
+
+        static void ShowPokemonList(byte[] list)
+        {
+
+            bool[] PokemonOwnedList = GetPokemonOwnedList(list);
+
+            Console.WriteLine("Owned:\r\n");
+
+            int count = 1;
+
+            for (int i = 0; i != 151; ++i)
+                if (PokemonOwnedList[i])
+                    if (count++ % 5 != 0 )
+                        Console.Write("{0}", PokemonNames[i].PadLeft(20));
+                    else
+                        Console.WriteLine("{0}", PokemonNames[i].PadLeft(20));
+
+            Console.WriteLine("\r\n\r\n");
+
+            bool[] PokemonUnownedList = GetPokemonUnownedList(list);
+
+            Console.WriteLine("Unowned:\r\n");
+
+            count = 1;
+
+            for (int i = 0; i != 151; ++i)
+                if (PokemonUnownedList[i])
+                    if (count++ % 5 != 0)
+                        Console.Write("{0}", PokemonNames[i].PadLeft(20));
+                    else
+                        Console.WriteLine("{0}", PokemonNames[i].PadLeft(20));
+
+
+        }
+    }
+}
